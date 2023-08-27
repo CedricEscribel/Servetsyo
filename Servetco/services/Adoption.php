f6147d6dbc24cc222a62fde076d15588c186c6ec
cescribel <cescribel21@gmail.com>
2023-08-27 15:56:00 +0800

    YAN NA

--- Servetco/services/Adoption.php
+++ Servetco/services/Adoption.php
@@ -330,14 +330,6 @@
     </div>  
     <!-- Footer End -->
 
-       <!-- The core Firebase JS SDK is always required and must be listed first -->
-       <script src="https://www.gstatic.com/firebasejs/8.6.8/firebase-app.js"></script>
-
-        <!-- TODO: Add SDKs for Firebase products that you want to use
-            https://firebase.google.com/docs/web/setup#available-libraries -->
-        <script src="https://www.gstatic.com/firebasejs/8.6.8/firebase-auth.js"></script>
-        <script src="https://www.gstatic.com/firebasejs/8.6.8/firebase-database.js"></script>
-
 
 
     <!-- JavaScript Libraries -->

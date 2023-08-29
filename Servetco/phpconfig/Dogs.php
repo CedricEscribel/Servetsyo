579577696fa12b5173d6aafcd38c90240773bfd9
cescribel <cescribel21@gmail.com>
2023-08-29 21:20:16 +0800

    save

--- Servetco/phpconfig/Dogs.php
+++ Servetco/phpconfig/Dogs.php
@@ -14,26 +14,55 @@
      $Gender = $_POST['Gender'];
      $CatchLocation = $_POST['CatchLocation'];
 
-      $DogImg = $_FILES["image"]["name"];
-      $tempname = $_FILES["image"]["tmp_name"];
-      $folder = "dogs/" . $DogImg;
+// Image to folder
+    $validImageExtension = ['jpg', 'jpeg', 'png'];
+    $imageExtension = explode('.', $fileName);
+    $imageExtension = strtolower(end($imageExtension));
+    if ( !in_array($imageExtension, $validImageExtension) ){
+      echo
+      "
+      <script>
+        alert('Invalid Image Extension');
+      </script>
+      ";
+    }
+    else if($fileSize > 1000000){
+      echo
+      "
+      <script>
+        alert('Image Size Is Too Large');
+      </script>
+      ";
+    }
+    else{
+      $newImageName = uniqid();
+      $newImageName .= '.' . $imageExtension;
+      move_uploaded_file($tmpName, 'img/' . $newImageName);
+      $sql = "INSERT INTO dogs(Name, DogInfo, Age, Gender, CatchLocation, DogImg, image) 
+      VALUES ('$Name','$DogInfo','$Age','$Gender','$CatchLocation', '$DogImg', $newImageName)";
+ 
+     // insert in database 
+       $Save = mysqli_query($con, $sql);
+      echo
+      "
+      <script>
+        alert('Successfully Added');
+        document.location.href = 'data.php';
+      </script>
+      ";
+      die(header("Location: ../Admindashboard/Adoption.php"));
+    }
+    // image send end
+    //  $sql = "INSERT INTO dogs(Name, DogInfo, Age, Gender, CatchLocation, DogImg) 
+    //  VALUES ('$Name','$DogInfo','$Age','$Gender','$CatchLocation', '$DogImg')";
 
-     $sql = "INSERT INTO dogs(Name, DogInfo, Age, Gender, CatchLocation, DogImg) 
-     VALUES ('$Name','$DogInfo','$Age','$Gender','$CatchLocation', '$DogImg')";
-
-    // insert in database 
-      $Save = mysqli_query($con, $sql);
-      if($Save)
-      {
-        die(header("Location: ../Admindashboard/Adoption.php"));
-      }
+    // // insert in database 
+    //   $Save = mysqli_query($con, $sql);
+      // if($Save)
+      // {
+      //   die(header("Location: ../Admindashboard/Adoption.php"));
+      // }
       
-      // Now let's move the uploaded image into the folder: image
-      if (move_uploaded_file($tempname, $folder)) {
-        echo "<h3> Image uploaded successfully!</h3>";
-      } else {
-        echo "<h3> Failed to upload image!</h3>";
-      }
   }
 
   function display_data(){
---
+++ sample/upload.sql
@@ -0,0 +1,59 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.4
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Dec 18, 2021 at 03:17 PM
+-- Server version: 10.4.17-MariaDB
+-- PHP Version: 8.0.2
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `upload`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tb_upload`
+--
+
+CREATE TABLE `tb_upload` (
+  `id` int(11) NOT NULL,
+  `name` varchar(50) NOT NULL,
+  `image` varchar(75) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `tb_upload`
+--
+ALTER TABLE `tb_upload`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `tb_upload`
+--
+ALTER TABLE `tb_upload`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;

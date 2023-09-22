f4ef022bafcaa92794ac3eaab7bb349538d95139
Smooothieee11 <nmce1011@gmail.com>
2023-09-22 14:59:03 +0800

    color change

--- Servetco/Admindashboard/design/header.php
+++ Servetco/Admindashboard/design/header.php
@@ -1,8 +1,21 @@
 <!-- Navbar Start -->
 <nav class="navbar sticky-top navbar-expand-lg bg-white navbar-light shadow-sm py-3 py-lg-0 px-3 px-lg-0">
     <a class="navbar-brand ms-lg-5">
-    <img src="../img/logo.jpg"  class="img-fluid" style="width: 80px; height: ms-auto;" alt="..." >
-    </a>    
+        <img src="../img/logo.jpg" class="img-fluid" style="width: 80px; height: ms-auto;" alt="...">
+    </a>
     <h1 style="margin-top: 18px; color: #378ACA; font-weight: bold;">SERVETSYO</h1>
-</nav>
 
+
+
+    <div class="collapse navbar-collapse" id="navbarCollapse" style="margin-right: 5%;" >
+        <div class="navbar-nav ms-auto py-0">
+            <div class="nav-item dropdown" >
+                <a href="#" class="nav-link dropdown-toggle active" data-bs-toggle="dropdown">Profile</a>
+                <div class="dropdown-menu m-0">
+                    <a href="profile.php" class="dropdown-item active">Profile</a>
+                    <a href="login.php" class="dropdown-item">Login</a>
+                </div>
+            </div>
+        </div>
+    </div>
+</nav>

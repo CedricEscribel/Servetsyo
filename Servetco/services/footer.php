65b8760dd228504a341ce134176b2debf77d442b
cescribel <cescribel21@gmail.com>
2023-09-25 00:10:24 +0800

    ewan

--- Servetco/services/footer.php
+++ Servetco/services/footer.php
@@ -1,3 +1,8 @@
+    <!-- JavaScript Libraries -->
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.min.js" integrity="sha384-Rx+T1VzGupg4BHQYs2gCW9It+akI2MM/mndMCy36UVfodzcJcF0GGLxZIzObiEfa" crossorigin="anonymous"></script>  
+    <!-- Template Javascript --> 
+
 <div class="footer container-fluid bg-light mt-5 py-5">
         <div class="container pt-5">
             <div class="row g-5">
@@ -22,3 +27,4 @@
             </div>
         </div>
     </div>  
+

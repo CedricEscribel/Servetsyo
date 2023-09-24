75d95df9c3aae7cd51b06faf3149b790b945ada0
cescribel <cescribel21@gmail.com>
2023-09-24 22:05:32 +0800

    Folders

--- Servetco/Admindashboard/Announcement.php
+++ Servetco/Admindashboard/Announcement.php
@@ -107,11 +107,7 @@ $all_events = $con->query($sql);
 </div>
 
 
-    <!-- jQuery CDN - Slim version (=without AJAX) -->
-    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.min.js" integrity="sha384-Rx+T1VzGupg4BHQYs2gCW9It+akI2MM/mndMCy36UVfodzcJcF0GGLxZIzObiEfa" crossorigin="anonymous"></script>  
-
+<?php include 'design/footer.php'; ?>
     <script src="Image.js"></script>
 </body>
 </html>

f02ff1a0b135e4ef0b2c226bda7fe113332ab8e8
Smooothieee <cescribel21@gmail.com>
2023-10-18 00:39:17 +0800

    clean

--- Servetco/Admindashboard/Announcement.php
+++ Servetco/Admindashboard/Announcement.php
@@ -33,84 +33,94 @@ if (isset($_SESSION['admin_id'])) {
           <h1>Announcement</h1>
         </div>
         <!-- Body design -->
-        <span>
-          <div class="Upload">
-            <form class="img_prv" enctype="multipart/form-data" method="POST" autocomplete="off">
-              <div class="Informations">
-                <div class="DogInput">
-                  <div class="DInput">
-                    <label for="Name">Title:</label>
-                    <input type="text" name="EventName" placeholder="Enter title:" required>
-                  </div>
-                  <div class="DInput" style="margin-top: 22px; margin-left:25px ;">
+
+        <div class="d-flex align-items-center flex-column mb-4 border border-success p-2 mb-2 border-opacity-50">
+          <form enctype="multipart/form-data" method="POST" autocomplete="off">
+
+            <div class="container">
+
+              <div class="col">
+                <div class="row my-2" style="max-width: 85%;">
+                  <label for="Name">Title:</label>
+                  <input type="text" name="EventName" placeholder="Enter title:" required>
+                </div>
+                <div class="row">
+                  <div class="col-5 my-2">
                     <label for="Date">Date:</label>
                     <input type="date" name="Dates" placeholder="Date" required>
                   </div>
-                  <div class="DInput" style="margin-top: 22px; margin-left:25px ;">
-                    <label for="Expired_date">Expired_date:</label>
+                  <div class="col-5 my-2">
+                    <label for="Expired date">Expired date:</label>
                     <input type="date" name="Expired_date" required>
                   </div>
                 </div>
-                <label for="Name" style="margin: 10px;">Event details:</label>
-                <div class="Doginput2">
-                  <div class="DInput">
-                    <textarea rows="4" cols="40" name="Details" placeholder="Event details"></textarea>
-                  </div>
+              </div>
 
-                  <input type="file" id="image-file" name="image" accept="image/*" value="" style="display: none">
-                  <div style="margin-left: 10%;" class="image-preview">
-                    <img src="../image/upload.png" width="50px" alt="Upload Dog Picture" id="image-preview">
-                    <a href="#" id="choose-file-btn">Choose Images</a>
-                  </div>
-                </div>
-                <div class="button" style="max-width: 33%;">
-                  <input style="color: white;" type="submit" name="submit" value="Post">
+              <div class="col" style="margin-left: 25%;">
+                <input type="file" id="image-file" name="image" accept="image/*" value="" style="display: none">
+                <div class="image-preview">
+                  <img src="../image/upload.png" alt="Upload Dog Picture" id="image-preview">
+                  <a href="#" id="choose-file-btn">Choose Images</a>
                 </div>
               </div>
 
-            </form>
-          </div>
-        </span>
-        <!-- Upload dog for adoption end -->
+            </div>
 
-        <div class="row border">
-          <?php
-          while ($row = $all_events->fetch_assoc()) {
-          ?>
-            <div class="col-lg-6 border border-success" style="margin: 2px;">
-              <div class="blog-item ">
-                <div class="row g-0 bg-light overflow-hidden">
-                  <div class="col-12 col-sm-5 h-100">
-                    <img src="EventsImg/<?php echo $row["image"]; ?>" width=200 title="<?php echo $row['image']; ?>">
-                  </div>
-                  <div class="col-12 col-sm-7 h-100 d-flex flex-column justify-content-center">
-                    <div class="p-4">
-                      <div class="d-flex mb-3">
-                        <small><i class="bi bi-calendar-date me-2"></i><?php echo $row["Dates"] ?> / <small style="color: red;"><?php echo $row["Expired_date"] ?></small></small>
-                      </div>
-                      <h5 class="text-uppercase mb-3"><?php echo $row["EventName"] ?></h5>
-                      <p class="fw-lighter lh-lg"><?php echo $row["Details"] ?></p>
-                    </div>
-                  </div>
+            <div class="col">
+              <label for="Name">Event details:</label>
+              <div>
+                <div>
+                  <textarea rows="4" cols="50" name="Details" placeholder="Event details"></textarea>
                 </div>
               </div>
             </div>
 
-          <?php
-          }
-          ?>
+
+            <div class="button">
+              <input style="color: white;" type="submit" name="submit" value="Post">
+            </div>
+          </form>
+        </div>
+
+
+        <div class="container">
+          <div class="row border">
+            <?php
+            while ($row = $all_events->fetch_assoc()) {
+            ?>
+              <div class="col-lg-6 border border-success" style="margin: 2px;">
+                <div class="blog-item ">
+                  <div class="row g-0 bg-light overflow-hidden">
+                    <div class="col-12 col-sm-5 h-100">
+                      <img src="EventsImg/<?php echo $row["image"]; ?>" width=200 title="<?php echo $row['image']; ?>">
+                    </div>
+                    <div class="col-12 col-sm-7 h-100 d-flex flex-column justify-content-center">
+                      <div class="p-4">
+                        <div class="d-flex mb-3">
+                          <small><i class="bi bi-calendar-date me-2"></i><?php echo $row["Dates"] ?> / <small style="color: red;"><?php echo $row["Expired_date"] ?></small></small>
+                        </div>
+                        <h5 class="text-uppercase mb-3"><?php echo $row["EventName"] ?></h5>
+                        <p class="fw-lighter lh-lg"><?php echo $row["Details"] ?></p>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+          </div>
+        <?php
+            }
+        ?>
         </div>
 
       </div>
-      </nav>
-    </div>
 
 
-    <?php include 'design/footer.php'; ?>
-    <script src="Image.js"></script>
-    </body>
 
-  <?php } else {
-  echo '<script>alert("Log in first")</script>';
-  echo '<script>window.location.href = "login.php";</script>';
-} ?>
+      <?php include 'design/footer.php'; ?>
+      <script src="Image.js"></script>
+      </body>
+
+    <?php } else {
+    echo '<script>alert("Log in first")</script>';
+    echo '<script>window.location.href = "login.php";</script>';
+  } ?>

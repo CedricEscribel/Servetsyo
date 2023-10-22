eb1596ce071b2f4ff7b4797e07061dbc9ddbc776
Smooothieee11 <nmce1011@gmail.com>
2023-10-22 13:41:00 +0800

    pull

--- Servetsyo/services/header.php
+++ Servetsyo/services/header.php
@@ -18,7 +18,7 @@
         <a href="../index.php" class="navbar-brand ms-lg-5">
             <img src="../img/logo.jpg" class="img-fluid" style="width: 90px; height: ms-auto;" alt="...">
         </a>
-        <div style="display: flex; margin-top: 3%;">
+        <div style="display: flex; ">
             <h2 style="margin-top: 18px; color: #378ACA; font-weight: bold;">SER</h2>
             <h2 style="margin-top: 18px; color: #4caf50; font-weight: bold;">VET</h2>
             <h2 style="margin-top: 18px; color: #378ACA; font-weight: bold;">SYO</h2>

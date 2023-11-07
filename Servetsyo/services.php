4dfc3d7c1c6ee99e30f78eb26fff5383a369affd
Smooothieee <cescribel21@gmail.com>
2023-11-07 20:13:57 +0800

    services

--- Servetsyo/services.php
+++ Servetsyo/services.php
@@ -4,37 +4,6 @@
             <h1 class="display-5 text-uppercase mb-0">Our Services</h1>
         </div>
         <div class="row g-5">
-
-            <div class="col-md-6">
-                <div class="service-item bg-light d-flex p-4">
-                    <div>
-                        <h5 class="text-uppercase mb-3">Artificial Insemination</h5>
-                        <p>Application form for Artificial Insemination for Large Ruminants</p>
-                        <a class="text-primary text-uppercase" href="services/Insemination.php">Artificial Insemination form<i class="bi bi-chevron-right"></i></a>
-                    </div>
-                </div>
-            </div>
-            <div class="col-md-6">
-                <div class="service-item bg-light d-flex p-4">
-                    <div>
-                        <h5 class="text-uppercase mb-3">Schedule form</h5>
-                        <p>Application form for Vaccination and Castration</p>
-                        <a class="text-primary text-uppercase" href="services/schedule.php">Send Schedule form<i class="bi bi-chevron-right"></i></a>
-                    </div>
-                </div>
-            </div>
-
-            <div class="col-md-6">
-                <div class="service-item bg-light d-flex p-4">
-                    <div>
-                        <h5 class="text-uppercase mb-3">Animal Rescue</h5>
-                        <p>Send a rescue form</p>
-                        <a class="text-primary text-uppercase" href="services/Rescue.php">Send Rescue form<i class="bi bi-chevron-right"></i></a>
-                    </div>
-                </div>
-            </div>
-
-
             <?php if (isset($_SESSION['RoleType']) && $_SESSION['RoleType'] == 'Barangay') { ?>
                 <div class="col-md-6">
                     <div class="service-item bg-light d-flex p-4">
@@ -45,9 +14,36 @@
                         </div>
                     </div>
                 </div>
+            <?php } else { ?>
+                <!-- Display other sections when not logged in or for other roles -->
+                <div class="col-md-6">
+                    <div class="service-item bg-light d-flex p-4">
+                        <div>
+                            <h5 class="text-uppercase mb-3">Artificial Insemination</h5>
+                            <p>Application form for Artificial Insemination for Large Ruminants</p>
+                            <a class="text-primary text-uppercase" href="services/Insemination.php">Artificial Insemination form<i class="bi bi-chevron-right"></i></a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-6">
+                    <div class="service-item bg-light d-flex p-4">
+                        <div>
+                            <h5 class="text-uppercase mb-3">Schedule form</h5>
+                            <p>Application form for Vaccination and Castration</p>
+                            <a class="text-primary text-uppercase" href="services/schedule.php">Send Schedule form<i class="bi bi-chevron-right"></i></a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-6">
+                    <div class="service-item bg-light d-flex p-4">
+                        <div>
+                            <h5 class="text-uppercase mb-3">Animal Rescue</h5>
+                            <p>Send a rescue form</p>
+                            <a class="text-primary text-uppercase" href="services/Rescue.php">Send Rescue form<i class="bi bi-chevron-right"></i></a>
+                        </div>
+                    </div>
+                </div>
             <?php } ?>
-
-
         </div>
     </div>
 </div>
